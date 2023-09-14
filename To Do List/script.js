9f8cd6e416e8ddff07f4ec11f218c4bed854257b
Harshvardhan Sharma <102682161+hvs1234@users.noreply.github.com>
2023-09-14 11:43:05 +0530

    Update script.js

--- To Do List/script.js
+++ To Do List/script.js
@@ -2,8 +2,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const taskInput = document.getElementById("task");
     const addTaskButton = document.getElementById("addTask");
     const taskList = document.getElementById("taskList");
-
-    // Function to add a new task
     function addTask() {
         const taskText = taskInput.value.trim();
         if (taskText !== "") {
@@ -14,21 +12,15 @@ document.addEventListener("DOMContentLoaded", function () {
             `;
             taskList.appendChild(li);
 
-            taskInput.value = ""; // Clear the input field
+            taskInput.value = "";
         }
     }
-
-    // Event listener for adding a task
     addTaskButton.addEventListener("click", addTask);
-
-    // Event delegation for deleting a task
     taskList.addEventListener("click", function (e) {
         if (e.target.classList.contains("delete")) {
             e.target.parentElement.remove();
         }
     });
-
-    // Event listener for adding a task when the Enter key is pressed
     taskInput.addEventListener("keyup", function (e) {
         if (e.key === "Enter") {
             addTask();
